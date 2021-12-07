e4ba585a1021cbfa5f908c2733ee480215eaf973
congxinhhe99 <conganhiikk9@gmail.com>
2021-12-07 21:23:48 +0700

    add home

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports.Pages = [
   {
-    template: './views/index.html',
-    filename: 'index.html',
-    js: ['index']
+    template: "./views/index.html",
+    filename: "index.html",
+    js: ["index"],
+  },
+
+  {
+    template: "./views/product-list.html",
+    filename: "product-list.html",
+    js: ["product-list"],
   },
   {
-    template: './views/product-list.html',
-    filename: 'product-list.html',
-    js: ['product-list']
+    template: "./views/blog.html",
+    filename: "blog.html",
+    js: ["blog"],
   },
-  {
-    template: './views/blog.html',
-    filename: 'blog.html',
-    js: ['blog']
-  },
-  
 ];
